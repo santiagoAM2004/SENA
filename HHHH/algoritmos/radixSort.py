c51ecfa7a2bc328f5de482f0558ca97668e3f2a8
unknown <SENA@ARMAPRRQDFSD324.FormacionSena.RED>
2021-10-06 17:31:33 -0500

    HHHHHHHHHHH

---
+++ HHHH/algoritmos/radixSort.py
@@ -0,0 +1,13 @@
+import random
+
+class RadixSort():
+    def __init__(self):
+        self.liststr = []
+
+    def disorganizadeList(self):
+        random_number = random.randint(10,100)
+        for i in range(random_number):
+            pass
+#####################################################
+HHHH = RadixSort()
+HHHH.disorganizadeList()
